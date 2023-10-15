d5756eac57854e846d3b09c0af25db100201b3fe
murugappansrija <srijamurugappan66@gmail.com>
2023-10-15 10:51:40 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const connectDB = async function () {
+    try {
+           await mongoose.connect(process.env.MONGODBURI,{
+  useNewUrlParser: "true",
+  useUnifiedTopology: "true"
+        });
+        console.log('db connection success')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default connectDB
