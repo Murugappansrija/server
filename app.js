f902dd39e96fde60b20aa9f335b7b2109cf9381a
murugappansrija <srijamurugappan66@gmail.com>
2023-10-26 15:51:43 +0530

    apps

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-import user from '../server/routes/user.js'
-import todo from '../server/routes/todo.js'
+import user from './routes/user.js'
+import todo from './routes/todo.js'
 
 app.use('/user', user)
 app.use('/todo', todo)
