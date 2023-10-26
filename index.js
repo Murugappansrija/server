e477caee9cee5cf5123be7562dfd58348790609e
murugappansrija <srijamurugappan66@gmail.com>
2023-10-26 15:34:07 +0530

    final

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import app from './app.js'
 import connectDB from "./config/db.js";
-import dotenv from 'dotenv'
-import cors from 'cors'
-dotenv.config()
 connectDB()
-app.use(cors())
 app.listen(4000, () => {
     console.log('server is up')
 })
