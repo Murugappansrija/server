09f02a5e0b8acfb10190c040c7898d7238cd9926
murugappansrija <srijamurugappan66@gmail.com>
2023-10-26 12:56:37 +0530

    changes

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 import express from "express";
 import cookieParser from 'cookie-parser'
-import cors from 'cors'
+
 
 import user from "./routes/user.js"
 import todo from './routes/todo.js'
 const app = express()
 
-app.use(cors())
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import app from './app.js'
 import connectDB from "./config/db.js";
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 connectDB()
+app.use(cors())
 app.listen(4000, () => {
     console.log('server is up')
 })
